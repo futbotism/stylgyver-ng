36bacca0289a4f3f74b268cd8675135d78710c7e
Andrew Alvino <alvino.aj@gmail.com>
2017-11-21 23:14:57 +0000

    Updated home page

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,9 +1,24 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sg-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('heroState', [
+      state('go-right', style({
+        // backgroundColor: '#cfd8dc',
+        transform: 'translateX(60%)'
+      })),
+      state('go-left', style({
+        // backgroundColor: '#cfd8dc',
+        transform: 'translateX(-60%)'
+      })),
+      transition('* => go-left', animate('1600ms ease-in-out')),
+      transition('* => go-right', animate('1600ms ease-in-out')),
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   @HostBinding('class.page-container') true;
